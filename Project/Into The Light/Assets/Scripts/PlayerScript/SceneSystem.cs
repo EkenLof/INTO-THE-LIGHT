7eb719a55741902e8cfcce704a0a9d94333264e2
Jonathan Ek <ekenlof@hotmail.com>
2022-11-07 14:11:31 +0100

    Test

--- Project/Into The Light/Assets/Scripts/PlayerScript/SceneSystem.cs
+++ Project/Into The Light/Assets/Scripts/PlayerScript/SceneSystem.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor;
+//using UnityEngine.UIElements;
 
 public class SceneSystem : MonoBehaviour
 {
@@ -12,15 +15,19 @@ public class SceneSystem : MonoBehaviour
     [SerializeField] bool actionNext = false;
     [SerializeField] bool actionPrev = false;
 
+    [Header ("Loading")]
+    AsyncOperation loadingOperation;
     public GameObject loadingScreenUi;
+    public Slider progressBar;
+    float progressValue;
+    public Text progressText;
 
     void Update()
     {
         bool sceneLoadKeys = Input.GetMouseButtonDown(0);
 
-        if (active && actionNext && sceneLoadKeys) LoadingScene(nextScene);
-        else if (active && actionPrev && sceneLoadKeys) LoadingScene(prevScene);
-        else loadingScreenUi.SetActive(false);
+        //if (active && actionNext && sceneLoadKeys) LoadingScene(nextScene);
+        //else if (active && actionPrev && sceneLoadKeys) LoadingScene(prevScene);
     }
 
     void OnTriggerEnter(Collider other)
@@ -31,9 +38,27 @@ public class SceneSystem : MonoBehaviour
     {
         if (other.gameObject.tag == "Player") active = false; 
     }
-    public void LoadingScene(string sceneName)
+    public void LoadingScene(int sceneName)
     {
-        if(active) loadingScreenUi.SetActive(true);
-        SceneManager.LoadSceneAsync(sceneName);
+        //loadingOperation = SceneManager.LoadSceneAsync(sceneName);
+        if (loadingOperation.isDone) loadingScreenUi.SetActive(false);
+
+        //if (active) loadingScreenUi.SetActive(true);
+        //////////StartCoroutine(LoadAsynchronously());
+        //SceneManager.LoadSceneAsync(sceneName);
+    }
+
+    IEnumerable LoadAsynchronously(int sceneName)
+    {
+        loadingOperation = SceneManager.LoadSceneAsync(sceneName);
+
+        loadingScreenUi.SetActive(true);
+
+        while (!loadingOperation.isDone)
+        {
+            
+
+            yield return null;
+        }
     }
 }
