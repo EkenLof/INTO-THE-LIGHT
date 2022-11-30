d8ab05b25590992a1ec71a91ac5f7f12687cd480
Sebastianmajlof <Sebastianmajlof@hotmail.com>
2022-11-30 17:16:27 +0100

    Shower added to project in bought assets and placed in scene

---
+++ Project/Into The Light/Assets/Bought Assets/SmallShowerSet/Scripts/Shower.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shower : MonoBehaviour
+{
+    public bool On = false;
+    public ParticleSystem effect;
+    public AudioSource aud;
+    Animator anim;
+ 
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (On)
+                On = false;
+            else
+                On = true;
+        }
+
+        if (On)
+        {
+            anim.SetBool("ShowerOn", true);
+            if (!effect.isEmitting)
+            {
+                effect.Play();
+                aud.Play();
+            }
+
+        }
+        else
+        {
+            anim.SetBool("ShowerOn", false);
+            if (effect.isEmitting)
+            {
+                effect.Stop();
+                aud.Stop();
+            }
+        }
+    }
+}
